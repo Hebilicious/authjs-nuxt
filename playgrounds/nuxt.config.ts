34f6ad42ce6a6619afb5be3465bd245593bfd6b1
Hebilicious <xsh4k3@gmail.com>
2023-05-12 03:04:11 +0700

    fix: bundle dependencies for pnpm compatibility

--- playgrounds/nuxt.config.ts
+++ playgrounds/nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    "../packages/authjs-nuxt/src/module"
+    // "../packages/authjs-nuxt/src/module"
+    "@hebilicious/authjs-nuxt"
   ],
   nitro: {
     routeRules: {
