0c369df47958b0acef4e0b7b3bcb49e29b8ce12d
AwesomeDude091 <58686898+AwesomeDude091@users.noreply.github.com>
2023-06-30 20:52:03 +0200

    Add getToken to server requests

--- packages/authjs-nuxt/src/runtime/lib/server.ts
+++ packages/authjs-nuxt/src/runtime/lib/server.ts
@@ -1,6 +1,7 @@
 import crypto from "node:crypto"
 import type { RuntimeConfig } from "nuxt/schema"
 import { Auth, skipCSRFCheck } from "@auth/core"
+import { getToken as authGetToken } from "@auth/core/jwt"
 import type { H3Event } from "h3"
 import { eventHandler, getRequestHeaders, getRequestURL } from "h3"
 import type { AuthConfig, Session } from "@auth/core/types"
@@ -18,6 +19,7 @@ if (!globalThis.crypto) {
  * This is the event handler for the catch-all route.
  * Everything can be customized by adding a custom route that takes priority over the handler.
  * @param options
+ * @param runtimeConfig
  * @returns
  */
 export function NuxtAuthHandler(options: AuthConfig, runtimeConfig: RuntimeConfig) {
@@ -53,4 +55,18 @@ export async function getServerSession(
   if (status === 200)
     return data
   throw new Error(data.message)
+}
+
+export function getToken<R extends boolean = false>({ event, secureCookie, secret, ...rest }: Omit<GetTokenParams<R>, "req"> & { event: H3Event }) {
+  return authGetToken({
+  // @ts-expect-error As our request is not a real next-auth request, we pass down only what's required for the method, as per code from https://github.com/nextauthjs/next-auth/blob/8387c78e3fef13350d8a8c6102caeeb05c70a650/packages/next-auth/src/jwt/index.ts#L68
+    req: {
+      cookies: parseCookies(event),
+      headers: getHeaders(event)
+    },
+    // see https://github.com/nextauthjs/next-auth/blob/8387c78e3fef13350d8a8c6102caeeb05c70a650/packages/next-auth/src/jwt/index.ts#L73
+    secureCookie: secureCookie || getServerOrigin(event).startsWith("https://"),
+    secret: secret || usedSecret,
+    ...rest
+  })
 }
