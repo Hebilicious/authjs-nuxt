c65f6ddf6d12c27bbdbfa4d0f3c402f52e9ce214
AwesomeDude091 <58686898+AwesomeDude091@users.noreply.github.com>
2023-06-30 21:21:14 +0200

    Finished patching function and added Documentation

--- packages/authjs-nuxt/src/runtime/lib/server.ts
+++ packages/authjs-nuxt/src/runtime/lib/server.ts
@@ -16,6 +16,10 @@ if (!globalThis.crypto) {
   })
 }
 
+export const isProduction = process.env.NODE_ENV === "production"
+
+let usedSecret: string | undefined
+
 /**
  * This is the event handler for the catch-all route.
  * Everything can be customized by adding a custom route that takes priority over the handler.
@@ -27,6 +31,17 @@ export function NuxtAuthHandler(options: AuthConfig, runtimeConfig: RuntimeConfi
   return eventHandler(async (event) => {
     options.trustHost ??= true
     options.skipCSRFCheck = skipCSRFCheck
+    usedSecret = options?.secret
+    if (!usedSecret) {
+      if (isProduction) {
+        throw new Error("No secret")
+      }
+      else {
+        // eslint-disable-next-line no-console
+        console.log("No secret!")
+        usedSecret = "secret"
+      }
+    }
     const request = await getRequestFromEvent(event)
     if (request.url.includes(".js.map")) return // Do not handle source maps
     checkOrigin(request, runtimeConfig)
@@ -58,14 +73,37 @@ export async function getServerSession(
   throw new Error(data.message)
 }
 
+function getServerOrigin(): string {
+  // Prio 1: Environment variable
+  const envOrigin = process.env.AUTH_ORIGIN
+  if (envOrigin)
+    return envOrigin
+
+  // Prio 2: Runtime configuration
+  const runtimeConfigOrigin = useRuntimeConfig().public.authJs.baseUrl
+  if (runtimeConfigOrigin)
+    return runtimeConfigOrigin
+
+  throw new Error("No Origin")
+}
+
+/**
+ * Returns the JWT Token defined
+ * @param event H3 Event
+ * @param secureCookie Whether the cookie is secure or not
+ * @param secret cookie encryption secret
+ * @param rest rest of the config
+ * @returns JWT Token
+ */
 export function getToken<R extends boolean = false>({ event, secureCookie, secret, ...rest }: Omit<GetTokenParams<R>, "req"> & { event: H3Event }) {
   return authGetToken({
     req: {
       cookies: parseCookies(event),
+      // @ts-expect-error type [name: string]: string can be mapped to String Record<String>
       headers: getHeaders(event)
     },
     // see https://github.com/nextauthjs/next-auth/blob/8387c78e3fef13350d8a8c6102caeeb05c70a650/packages/next-auth/src/jwt/index.ts#L73
-    secureCookie: secureCookie || getServerOrigin(event).startsWith("https://"),
+    secureCookie: secureCookie || getServerOrigin().startsWith("https://"),
     secret: secret || usedSecret,
     ...rest
   })
