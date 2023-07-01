995339003d13b8eb18e284b5ca391fb6e5615a1f
Hebilicious <xsh4k3@gmail.com>
2023-07-01 14:44:07 +0700

    chore: playground postinstall scripts

--- playgrounds/github/nuxt.config.ts
+++ playgrounds/github/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    "../packages/authjs-nuxt/src/module"
+    "../packages/authjs-nuxt/src/module.ts"
     // "@hebilicious/authjs-nuxt"
   ],
   authJs: {
