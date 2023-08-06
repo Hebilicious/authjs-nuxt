44f8d824a250e065a573a21c58d4ba0da05e8069
Heb <xsh4k3@gmail.com>
2023-08-07 03:25:19 +0700

    feat: add signin types (#62)

--- packages/authjs-nuxt/src/runtime/lib/client.ts
+++ packages/authjs-nuxt/src/runtime/lib/client.ts
@@ -1,4 +1,4 @@
-import type { BuiltInProviderType, RedirectableProviderType } from "@auth/core/providers"
+import type { BuiltInProviderType, Provider, RedirectableProviderType } from "@auth/core/providers"
 import type { Session } from "@auth/core/types"
 import { makeNativeHeadersFromCookieObject } from "../utils"
 import { useAuth } from "../composables/useAuth"
@@ -31,6 +31,10 @@ async function postToInternal({
   })
   return response
 }
+
+type SignInResponse = Awaited<ReturnType<typeof postToInternal>>
+type ProviderID<P> = LiteralUnion<P extends RedirectableProviderType ? P | BuiltInProviderType : BuiltInProviderType>
+
 /**
  * Client-side method to initiate a signin flow
  * or send the user to the signin page listing all possible providers.
@@ -38,44 +42,63 @@ async function postToInternal({
  *
  * [Documentation](https://next-auth.js.org/getting-started/client#signin)
  */
+
 export async function signIn<P extends RedirectableProviderType | undefined = undefined>(
-  providerId?: LiteralUnion<
-    P extends RedirectableProviderType ? P | BuiltInProviderType : BuiltInProviderType
-  >,
+  providerId?: ProviderID<P>,
+  options?: SignInOptions & { redirect: true },
+  authorizationParams?: SignInAuthorizationParams
+): Promise<void>
+
+export async function signIn<P extends RedirectableProviderType | undefined = undefined>(
+  providerId?: ProviderID<P>,
+  options?: SignInOptions & { redirect: false },
+  authorizationParams?: SignInAuthorizationParams
+): Promise<SignInResponse>
+
+export async function signIn<P extends RedirectableProviderType | undefined = undefined>(
+  providerId?: ProviderID<P>,
   options?: SignInOptions,
   authorizationParams?: SignInAuthorizationParams
-) {
-  const { callbackUrl = window.location.href, redirect = true } = options ?? {}
+): Promise<void | SignInResponse> {
+  const { status } = useAuth()
+  try {
+    status.value = "loading"
+    const { callbackUrl = window.location.href, redirect = true } = options ?? {}
 
-  // TODO: Support custom providers
-  const isCredentials = providerId === "credentials"
-  const isEmail = providerId === "email"
-  const isSupportingReturn = isCredentials || isEmail
+    // TODO: Support custom providers
+    const isCredentials = providerId === "credentials"
+    const isEmail = providerId === "email"
+    const isSupportingReturn = isCredentials || isEmail
 
-  // TODO: Handle custom base path
-  const signInUrl = `/api/auth/${isCredentials ? "callback" : "signin"}/${providerId}`
-  const _signInUrl = `${signInUrl}?${new URLSearchParams(authorizationParams)}`
+    // TODO: Handle custom base path
+    const signInUrl = `/api/auth/${isCredentials ? "callback" : "signin"}/${providerId}`
+    const _signInUrl = `${signInUrl}?${new URLSearchParams(authorizationParams)}`
 
-  // TODO: Handle custom base path
+    // TODO: Handle custom base path
 
-  // We check the origin so CSRF check is un-necessary
-  // const { data: csrf } = await useFetch<{ csrfToken: string }>("/api/auth/csrf")
-  // const csrfToken = csrf?.value?.csrfToken
-  // if (!csrfToken) throw new Error("CSRF token not found")
+    // We check the origin so CSRF check is un-necessary
+    // const { data: csrf } = await useFetch<{ csrfToken: string }>("/api/auth/csrf")
+    // const csrfToken = csrf?.value?.csrfToken
+    // if (!csrfToken) throw new Error("CSRF token not found")
 
-  const response = await postToInternal({ url: _signInUrl, options, callbackUrl })
-  const url = response?._data?.url ?? null
-  const error = url ? new URL(url).searchParams.get("error") : null
-  if (error) throw new Error(error)
-  if (redirect || !isSupportingReturn) {
-    // TODO: Do not redirect for Credentials and Email providers by default in next major
-    const to = url ?? callbackUrl
-    await navigateTo(to, { external: true })
-    // If url contains a hash, the browser does not reload the page. We reload manually
-    if (to?.includes("#")) reloadNuxtApp({ persistState: true })
-    return
+    const response = await postToInternal({ url: _signInUrl, options, callbackUrl })
+    const url = response?._data?.url ?? null
+    const error = url ? new URL(url).searchParams.get("error") : null
+    if (error) throw new Error(error)
+    if (redirect || !isSupportingReturn) {
+      // TODO: Do not redirect for Credentials and Email providers by default in next major
+      const to = url ?? callbackUrl
+      await navigateTo(to, { external: true })
+      // If url contains a hash, the browser does not reload the page. We reload manually
+      if (to?.includes("#")) reloadNuxtApp({ persistState: true })
+      return
+    }
+    return response
   }
-  return response
+  catch (error) {
+    status.value = "error"
+    throw error
+  }
 }
 
 /**
@@ -85,27 +108,35 @@ export async function signIn<P extends RedirectableProviderType | undefined = un
  * [Documentation](https://next-auth.js.org/getting-started/client#signout)
  */
 export async function signOut(options?: SignOutParams) {
-  const { callbackUrl = window.location.href } = options ?? {}
-  // TODO: Custom base path
-  const data = await $fetch<{ url: string }>("/api/auth/signout", {
-    method: "post",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      "X-Auth-Return-Redirect": "1"
-    },
-    body: new URLSearchParams({
-      callbackUrl
+  const { status } = useAuth()
+  try {
+    status.value = "unauthenticated"
+    const { callbackUrl = window.location.href } = options ?? {}
+    // TODO: Custom base path
+    const data = await $fetch<{ url: string }>("/api/auth/signout", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "X-Auth-Return-Redirect": "1"
+      },
+      body: new URLSearchParams({
+        callbackUrl
+      })
     })
-  })
-  // Remove the session
-  useAuth().removeSession()
+    // Remove the session
+    useAuth().removeSession()
 
-  // Navigate back to where we are.
-  const url = data?.url ?? callbackUrl
-  // await navigateTo(new URL(url).pathname, { replace: true })
-  await useRouter().push({ path: new URL(url).pathname, force: true }) // navigateTo doesn't accept force
-  // If url contains a hash, the browser does not reload the page. We reload manually
-  if (url.includes("#")) reloadNuxtApp({ persistState: true })
+    // Navigate back to where we are.
+    const url = data?.url ?? callbackUrl
+    // await navigateTo(new URL(url).pathname, { replace: true })
+    await useRouter().push({ path: new URL(url).pathname, force: true }) // navigateTo doesn't accept force
+    // If url contains a hash, the browser does not reload the page. We reload manually
+    if (url.includes("#")) reloadNuxtApp({ persistState: true })
+  }
+  catch (error) {
+    status.value = "error"
+    throw error
+  }
 }
 
 /**
@@ -113,10 +144,8 @@ export async function signOut(options?: SignOutParams) {
  * See [OAuth Sign In](https://authjs.dev/guides/basics/pages#oauth-sign-in) for more details.
  */
 export async function getProviders() {
-  const data = await $fetch<Record<string, unknown>[]>("/api/auth/providers")
-  return data
+  return $fetch<Record<string, Provider>[]>("/api/auth/providers")
 }
-
 /**
  * Verify if the user session is still valid
  * TODO: Any type of refresh logic should be handled here
