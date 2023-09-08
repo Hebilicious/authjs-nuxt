a5a8cff1b452688af5386d92d1ae07a921db3ff2
Heb <xsh4k3@gmail.com>
2023-09-09 02:21:36 +0700

    chore: migrate to bun (#91)

--- packages/authjs-nuxt/src/nuxt-types.d.ts
+++ packages/authjs-nuxt/src/nuxt-types.d.ts
@@ -1,12 +1,15 @@
 import type { ModuleOptions } from "./module";
 
 declare module "@nuxt/schema" {
-    interface RuntimeConfig {
-      authJs: ModuleOptions
-    }
     interface PublicRuntimeConfig {
       authJs: ModuleOptions
     }
+}
+
+declare module "nuxt/schema" {
+  interface PublicRuntimeConfig {
+    authJs: ModuleOptions
+  }
 }
 
 export {}
