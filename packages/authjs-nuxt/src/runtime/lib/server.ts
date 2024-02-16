09571df098c76af7c9b1005f90aec5c155335c35
Jeffrey Leon <jals1212@gmail.com>
2024-02-16 18:19:32 -0400

    splitted token cookie handling

--- packages/authjs-nuxt/src/runtime/lib/server.ts
+++ packages/authjs-nuxt/src/runtime/lib/server.ts
@@ -1,10 +1,10 @@
-import type { RuntimeConfig } from "nuxt/schema"
 import { Auth, skipCSRFCheck } from "@auth/core"
+import { getToken } from "@auth/core/jwt"
+import type { AuthConfig, Session } from "@auth/core/types"
 import type { H3Event } from "h3"
 import { eventHandler, getRequestHeaders, getRequestURL } from "h3"
-import type { AuthConfig, Session } from "@auth/core/types"
-import { getToken } from "@auth/core/jwt"
-import { checkOrigin, getAuthJsSecret, getRequestFromEvent, getServerOrigin, makeCookiesFromCookieString } from "../utils"
+import type { RuntimeConfig } from "nuxt/schema"
+import { checkOrigin, getAuthJsSecret, getRequestFromEvent, getServerOrigin, makeObjectFromNativeHeader } from "../utils"
 
 if (!globalThis.crypto) {
   // eslint-disable-next-line no-console
@@ -27,6 +27,7 @@ if (!globalThis.crypto) {
  */
 export function NuxtAuthHandler(options: AuthConfig, runtimeConfig: RuntimeConfig) {
   return eventHandler(async (event) => {
+    options.secret ||= getAuthJsSecret(runtimeConfig)
     options.trustHost ??= true
     options.skipCSRFCheck = skipCSRFCheck
     const request = await getRequestFromEvent(event)
@@ -65,8 +66,8 @@ export async function getServerSession(
  */
 export async function getServerToken(event: H3Event, options: AuthConfig, runtimeConfig?: Partial<RuntimeConfig>) {
   const response = await getServerSessionResponse(event, options)
-  const cookies = Object.fromEntries(response.headers.entries())
-  const parsedCookies = makeCookiesFromCookieString(cookies["set-cookie"])
+  const cookies = response.headers
+  const parsedCookies = makeObjectFromNativeHeader(cookies)
   const parameters = {
     req: {
       cookies: parsedCookies,
@@ -74,7 +75,7 @@ export async function getServerToken(event: H3Event, options: AuthConfig, runtim
     },
     // see https://github.com/nextauthjs/next-auth/blob/a79774f6e890b492ae30201f24b3f7024d0d7c9d/packages/core/src/jwt.ts
     secureCookie: getServerOrigin(event, runtimeConfig).startsWith("https://"),
-    secret: getAuthJsSecret(options)
+    secret: options.secret || getAuthJsSecret(runtimeConfig)
   }
   return getToken(parameters)
 }
